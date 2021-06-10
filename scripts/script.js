e1c5f73a2d998cb21e1967dfb03f2a86d644e555
JudeTreanor <85668506+JudeTreanor@users.noreply.github.com>
2021-06-10 12:08:01 +0200

    number reeeeeee

--- scripts/script.js
+++ scripts/script.js
@@ -1,30 +1,32 @@
 let square = document.querySelector(".square");
 let moveBy = 10;
+let score = 0;
+document.querySelector(".square").onclick = function () {
+  document.querySelector(".square").style.display = "none";
+  score++;
+  score = document.querySelector("score");
+  score.innerHTML = score;
+};
 
-document.getElementById("square").onclick=function(){
-    document.getElementById("square").style.display="none";
-}
-
-window.addEventListener('load', () => {
-    square.style.position = 'absolute';
-    square.style.left = 0;
-    square.style.top = 0;
+window.addEventListener("load", () => {
+  square.style.position = "absolute";
+  square.style.left = 0;
+  square.style.top = 0;
 });
- 
- 
-window.addEventListener('keyup', (e) => {
-    switch (e.key) {
-        case 'ArrowLeft':
-            square.style.left = parseInt(square.style.left) - moveBy + 'px';
-            break;
-        case 'ArrowRight':
-            square.style.left = parseInt(square.style.left) + moveBy + 'px';
-            break;
-        case 'ArrowUp':
-            square.style.top = parseInt(square.style.top) - moveBy + 'px';
-            break;
-        case 'ArrowDown':
-            square.style.top = parseInt(square.style.top) + moveBy + 'px';
-            break;
-    }
+
+window.addEventListener("keyup", (e) => {
+  switch (e.key) {
+    case "ArrowLeft":
+      square.style.left = parseInt(square.style.left) - moveBy + "px";
+      break;
+    case "ArrowRight":
+      square.style.left = parseInt(square.style.left) + moveBy + "px";
+      break;
+    case "ArrowUp":
+      square.style.top = parseInt(square.style.top) - moveBy + "px";
+      break;
+    case "ArrowDown":
+      square.style.top = parseInt(square.style.top) + moveBy + "px";
+      break;
+  }
 });
